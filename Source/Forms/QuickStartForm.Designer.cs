baeacfd21cba3c1d61b4c1e00131877a65e4b029
Aurelitec <develop@aurelitec.com>
2017-09-06 18:47:03 +0300

    Update app color in Quick Start form

--- Source/Forms/QuickStartForm.Designer.cs
+++ Source/Forms/QuickStartForm.Designer.cs
@@ -45,13 +45,13 @@
             // topAdvicePanel
             // 
             this.topAdvicePanel.ColumnCount = 7;
-            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.topAdvicePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
             this.topAdvicePanel.Controls.Add(this.pauseLabel, 5, 0);
             this.topAdvicePanel.Controls.Add(this.label5, 4, 0);
             this.topAdvicePanel.Controls.Add(this.label4, 3, 0);
@@ -61,24 +61,26 @@
             this.topAdvicePanel.Controls.Add(this.closeButton, 6, 1);
             this.topAdvicePanel.Controls.Add(this.ShowAtStartupCheckBox, 0, 1);
             this.topAdvicePanel.Controls.Add(this.pictureBox1, 1, 0);
-            this.topAdvicePanel.Location = new System.Drawing.Point(20, 20);
+            this.topAdvicePanel.Location = new System.Drawing.Point(16, 16);
+            this.topAdvicePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.topAdvicePanel.Name = "topAdvicePanel";
             this.topAdvicePanel.RowCount = 2;
-            this.topAdvicePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.topAdvicePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
-            this.topAdvicePanel.Size = new System.Drawing.Size(1050, 194);
+            this.topAdvicePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.topAdvicePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.topAdvicePanel.Size = new System.Drawing.Size(840, 155);
             this.topAdvicePanel.TabIndex = 3;
             // 
             // pauseLabel
             // 
-            this.pauseLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(137)))), ((int)(((byte)(27)))));
+            this.pauseLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.pauseLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pauseLabel.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pauseLabel.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.pauseLabel.Location = new System.Drawing.Point(753, 0);
+            this.pauseLabel.Location = new System.Drawing.Point(602, 0);
+            this.pauseLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.pauseLabel.Name = "pauseLabel";
-            this.pauseLabel.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.pauseLabel.Size = new System.Drawing.Size(144, 150);
+            this.pauseLabel.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.pauseLabel.Size = new System.Drawing.Size(116, 120);
             this.pauseLabel.TabIndex = 8;
             this.pauseLabel.Text = "To pause and hide the drape to the taskbar notification area, double-click or pre" +
     "ss Ctrl+F11.";
@@ -86,14 +88,15 @@
             // 
             // label5
             // 
-            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(114)))), ((int)(((byte)(157)))));
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(17)))), ((int)(((byte)(28)))));
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label5.Location = new System.Drawing.Point(603, 0);
+            this.label5.Location = new System.Drawing.Point(482, 0);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.label5.Size = new System.Drawing.Size(144, 150);
+            this.label5.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label5.Size = new System.Drawing.Size(116, 120);
             this.label5.TabIndex = 7;
             this.label5.Text = "To view what\'s behind the opaque drape, click the left mouse button or press the " +
     "Shift key.";
@@ -101,28 +104,30 @@
             // 
             // label4
             // 
-            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(87)))), ((int)(((byte)(6)))));
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label4.Location = new System.Drawing.Point(453, 0);
+            this.label4.Location = new System.Drawing.Point(362, 0);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.label4.Size = new System.Drawing.Size(144, 150);
+            this.label4.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label4.Size = new System.Drawing.Size(116, 120);
             this.label4.TabIndex = 6;
             this.label4.Text = "To delete a focus area, press the Delete key while its border is visible.";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label3
             // 
-            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(173)))), ((int)(((byte)(234)))));
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(17)))), ((int)(((byte)(28)))));
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.label3.Location = new System.Drawing.Point(303, 0);
+            this.label3.Location = new System.Drawing.Point(242, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.label3.Size = new System.Drawing.Size(144, 150);
+            this.label3.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Size = new System.Drawing.Size(116, 120);
             this.label3.TabIndex = 5;
             this.label3.Text = "To move or resize a focus area, bring the mouse near its edges and use the appear" +
     "ing border.";
@@ -130,14 +135,15 @@
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(173)))), ((int)(((byte)(234)))));
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(17)))), ((int)(((byte)(28)))));
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(2, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.label1.Size = new System.Drawing.Size(144, 150);
+            this.label1.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Size = new System.Drawing.Size(116, 120);
             this.label1.TabIndex = 3;
             this.label1.Text = "To add a new transparent focus area, draw a rectangle with the mouse or a finger." +
     "";
@@ -145,14 +151,15 @@
             // 
             // label7
             // 
-            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(173)))), ((int)(((byte)(234)))));
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(17)))), ((int)(((byte)(28)))));
             this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label7.Location = new System.Drawing.Point(903, 0);
+            this.label7.Location = new System.Drawing.Point(722, 0);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.label7.Size = new System.Drawing.Size(144, 150);
+            this.label7.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label7.Size = new System.Drawing.Size(116, 120);
             this.label7.TabIndex = 9;
             this.label7.Text = "For more options, right click or press Escape to get the menu.";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -164,9 +171,10 @@
             this.closeButton.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
             this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.closeButton.ForeColor = System.Drawing.Color.White;
-            this.closeButton.Location = new System.Drawing.Point(972, 158);
+            this.closeButton.Location = new System.Drawing.Point(778, 127);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(75, 34);
+            this.closeButton.Size = new System.Drawing.Size(60, 27);
             this.closeButton.TabIndex = 10;
             this.closeButton.Text = "Close";
             this.closeButton.UseVisualStyleBackColor = true;
@@ -179,9 +187,10 @@
             this.ShowAtStartupCheckBox.Checked = true;
             this.ShowAtStartupCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
             this.ShowAtStartupCheckBox.ForeColor = System.Drawing.Color.White;
-            this.ShowAtStartupCheckBox.Location = new System.Drawing.Point(3, 165);
+            this.ShowAtStartupCheckBox.Location = new System.Drawing.Point(2, 132);
+            this.ShowAtStartupCheckBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ShowAtStartupCheckBox.Name = "ShowAtStartupCheckBox";
-            this.ShowAtStartupCheckBox.Size = new System.Drawing.Size(133, 21);
+            this.ShowAtStartupCheckBox.Size = new System.Drawing.Size(103, 17);
             this.ShowAtStartupCheckBox.TabIndex = 11;
             this.ShowAtStartupCheckBox.Text = "Show at start-up";
             this.ShowAtStartupCheckBox.UseVisualStyleBackColor = true;
@@ -190,28 +199,29 @@
             // 
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = global::CinemaDrape.Properties.Resources.CinemaDrapeQuickStartFocusAreaV2;
-            this.pictureBox1.Location = new System.Drawing.Point(153, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.pictureBox1.Image = global::CinemaDrape.Properties.Resources.CinemaDrapeQuickStartFocusAreaV3;
+            this.pictureBox1.Location = new System.Drawing.Point(122, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(144, 150);
+            this.pictureBox1.Size = new System.Drawing.Size(116, 120);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
             // QuickStartForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1093, 234);
+            this.ClientSize = new System.Drawing.Size(874, 187);
             this.Controls.Add(this.topAdvicePanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Location = new System.Drawing.Point(0, 10);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "QuickStartForm";
-            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Padding = new System.Windows.Forms.Padding(8, 8, 8, 8);
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "CinemaDrape Quick Start";
             this.TopMost = true;
